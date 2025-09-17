eee2f7db773efb76a3a79d4f05f8344639648a05
Eloir <eloir.csem@gmail.com>
2025-09-17 15:13:51 -0300

    att1

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 text-center">
       <h1 className="text-4xl font-bold text-blue-600">Bem-vindo à C-SEM</h1>
       <p className="mt-4 text-lg text-gray-700">
-        CONSTANTINO SOLUÇÕES ENGENHARIA E MECÂNICA
+        ELOIR CONSTANTINO SOLUÇÕES ENGENHARIA E MECÂNICA
       </p>
       <a
         href="/contato"
